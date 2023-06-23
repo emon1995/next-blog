290bf83793675936e0e592f1b009e1c199a6bd60
emon1995 <emonhossain995@gmail.com>
2023-06-23 19:55:52 +0600

    basic route setup

---
+++ src/app/blog/[id]/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SingleBlogPage = () => {
+    return (
+        <div>
+            <h1>SingleBlogPage</h1>
+        </div>
+    );
+};
+
+export default SingleBlogPage;
---
+++ src/app/dashboard/logs/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LogsPage = () => {
+    return (
+        <div>
+            <h1>LogsPage</h1>
+        </div>
+    );
+};
+
+export default LogsPage;
